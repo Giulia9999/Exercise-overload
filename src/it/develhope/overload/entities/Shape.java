760e9626e277f960f36096cb7ee33f286cc4e9f7
Giulia <giuliac35@gmail.com>
2023-01-13 14:36:24 +0100

    Create method overload for square shape

--- src/it/develhope/overload/entities/Shape.java
+++ src/it/develhope/overload/entities/Shape.java
@@ -2,7 +2,7 @@ package it.develhope.overload.entities;
 
 public class Shape {
     public String shapeName;
-    public String numberOfEdges;
+    public int numberOfEdges;
 
     public Shape(){
         System.out.println("Shape object created");
@@ -10,13 +10,24 @@ public class Shape {
     }
 
     /**
-     * Overload shape constructor 1
+     * Overload shape constructor 1 (Circle)
      * @param circleName name of the shape
      * @param radius radius of the circle
      */
     public Shape(String circleName, double radius){
         System.out.println("Circle object created");
         this.shapeName = circleName;
+    }
+
+    /**
+     * Overload shape constructor 2 (Square)
+     * @param edges edges of the square
+     * @param edgeLength the square edge lenght
+     */
+    public Shape(String squareName,int edges, double edgeLength){
+        System.out.println("Square object created");
+        this.shapeName = squareName;
+        this.numberOfEdges = edges;
     }
 
 
