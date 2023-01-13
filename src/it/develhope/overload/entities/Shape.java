1dee0c4394bf84a0428908afe53ea18064b18847
Giulia <giuliac35@gmail.com>
2023-01-13 14:47:06 +0100

    Create overload Shape constructor for rectangle

--- src/it/develhope/overload/entities/Shape.java
+++ src/it/develhope/overload/entities/Shape.java
@@ -10,7 +10,7 @@ public class Shape {
     }
 
     /**
-     * Overload shape constructor 1 (Circle)
+     * Overload Shape constructor 1 (Circle)
      * @param circleName name of the shape
      * @param radius radius of the circle
      */
@@ -20,13 +20,27 @@ public class Shape {
     }
 
     /**
-     * Overload shape constructor 2 (Square)
+     * Overload Shape constructor 2 (Square)
+     * @param squareName The name of the shape
      * @param edges edges of the square
      * @param edgeLength the square edge lenght
      */
     public Shape(String squareName,int edges, double edgeLength){
         System.out.println("Square object created");
         this.shapeName = squareName;
+        this.numberOfEdges = edges;
+    }
+
+    /**
+     * Overload Shape constructor 3 (rectangle)
+     * @param rectangleName The name of the shape
+     * @param edges Edges of the rectangle
+     * @param e1 First rectangle edge
+     * @param e2 Second rectangle edge
+     */
+    public Shape(String rectangleName,int edges, double e1, double e2){
+        System.out.println("Rectangle object created");
+        this.shapeName = rectangleName;
         this.numberOfEdges = edges;
     }
 
